22aeccb638cc76b1474ba3edd6e84c2324b30d65
Casey Deen <deen@mpia.de>
2015-08-12 17:47:22 +0200

    modified README

--- nocallback.f
+++ nocallback.f
@@ -1,13 +1,5 @@
       subroutine barney
 
-cf2py intent(callback, hide) fred 
-
-cf2py intent(callback, hide) bambam
-
-      external fred
-      external bambam
-
-      
       write (*,*) "barney"
 
       call test
